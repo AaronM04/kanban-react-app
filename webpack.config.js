cd4f2f3271f8aa9e89e82b3d6f6e4aaca10ada40
Aaron Miller <aaron.miller04@gmail.com>
2016-05-30 15:34:32 -0700

    add webpack-merge (thru p. 23)

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
-const path = require('path');
+const path  = require('path');
+const merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app:   path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given that it's convenient with more complex
   // configurations.
@@ -17,3 +19,12 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+// Default configuration
+if (TARGET === 'start' || !target) {
+  module.exports = merge(common, {});
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
